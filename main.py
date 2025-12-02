b8773dae1038db9482ab7aefc1c2408f06fdbee3
Czrvillareal <villareal.czarjohn@gmail.com>
2025-12-02 15:26:44 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
-# import sqlite3
-# import sys
+import sqlite3
+import sys
+import test
 
-# with sqlite3.connect("exam_db.db") as connection:
-#     cur = connection.cursor()
+with sqlite3.connect("exam_db.db") as connection:
+    cur = connection.cursor()
 
 
-# householdHeadName = input("Enter the name of the household head: ")
-# cur.execute("""
-#     SELECT name
-#     FROM sqlite_master
-#     WHERE type='table' AND name=?;""", (householdHeadName,))
+householdHeadName = input("Enter the name of the household head: ")
+cur.execute("""
+    SELECT name
+    FROM sqlite_master
+    WHERE type='table' AND name=?;""", (householdHeadName,))
 
-# tableExists = bool(cur.fetchone())
+tableExists = bool(cur.fetchone())
 
-# if tableExists == False:
-#     # print(tableExists)
-#     cur.execute("CREATE TABLE "+householdHeadName+
-#                 "(ID int NOT NULL, Name varchar(50), Relationship int(1), Age int(3), Employment_Status int(1), Monthly_Salary float(6,2), PRIMARY KEY(ID) )"
-#                 )
-# # else:
-# #     cur.execute()
+if tableExists == False:
+    # print(tableExists)
+    cur.execute("CREATE TABLE "+householdHeadName+
+                "(ID int NOT NULL, Name varchar(50), Relationship int(1), Age int(3), Employment_Status int(1), Monthly_Salary float(6,2), PRIMARY KEY(ID) )"
+                )
+# else:
+#     cur.execute()
 
 numberOfHouseholdMembers = int(input("Enter the number of household members: "))
 
@@ -32,16 +33,19 @@ if answer1 == "1":
         answer2 = int(input("How many more do you want to add?"))
         for i in range(answer2):
             listOfHouseholdMembers.append(input("Enter extra household member name: ")) 
-
+        else:
+            print(listOfHouseholdMembers)
+            test.name()
         
 elif answer1 == "2":
     
     print(listOfHouseholdMembers)
     for i in range(len(listOfHouseholdMembers)):
         print("Enter the following information for Household Member(left to right):")
-        name = input()
-
+        test.name()
 else:
-    print()
+    print("Invalid Input.")
+
+cur.execute("INSERT INTO "+householdHeadName+" VALUES "+name+" "+relationship+" "+age+" "+sex+" "+employmentStatus+" "+salary+" ")
 
 # print(listOfHouseholdMembers)
--- test.py
+++ test.py
@@ -2,7 +2,6 @@ def name():
     name = input("Please re-enter the name: ")
     if ((len(name) < 2) or (len(name) > 50)):
         print("Name should atleast be 2 characters long or less than 50 characters")
-        
     else:
         relationship()
 
@@ -19,7 +18,43 @@ def relationship():
         relationship()
     
     else:
-        print("nice")
+        age()
 
-name()
+def age():
+    age = int(input("Please Enter Age: "))
+    if age < 0:
+        print("Invalid Number")
+    elif age > 150:
+        print("Invalid Number")
+    else:
+        sex()
+
+def sex():
+    sex = input("Sex(M/F): ")
+    if sex != ("M" or "F"):
+        print("Invalid Input.")
+    else:
+        employmentStatus()
+    
+def employmentStatus():
+    employmentStatus = int(input("Are you 0(Unemployed) or 1(Employed): "))
+    if employmentStatus != (0 or 1):
+        print("Invalid Input.")
+    
+    elif employmentStatus == 0:
+        salary = 0
+    
+    else: 
+        salary()
+    
+def salary():
+
+    salary = float(input("What is your monthly salary? "))
+    if (salary >= 1000.00) or (salary <= 500000.00):
+        return
+    else:
+        print("Invalid Salary??")
+        
+    
+
         
